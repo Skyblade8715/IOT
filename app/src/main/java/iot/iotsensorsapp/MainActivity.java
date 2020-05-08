688e271f5142413e2b27be9b2d1ebc558af03064
Skyblade <piotr8715@gmail.com>
2020-05-08 19:01:24 +0200

    Complete config files push. Small review of code

--- app/src/main/java/iot/iotsensorsapp/MainActivity.java
+++ app/src/main/java/iot/iotsensorsapp/MainActivity.java
@@ -5,10 +5,7 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
 import android.os.Bundle;
-import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,7 +14,6 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.UiThread;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
